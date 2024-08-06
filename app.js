91d93214673d01af6e5d029511b29265d254bfb9
Diyarose <dhiyarosedavis@gmail.com>
2024-08-06 20:32:32 +0530

    created

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const cors=require("cors")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const loginModel = require("./models/admin")
 
-const app=express()
+const app = express()
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://dhiya04:dhiyafisat@cluster0.wspdqan.mongodb.net/patientdb?retryWrites=true&w=majority&appName=Cluster0")
-app.get("/test",(req,res)=>{
-    res.json({"status":"success"})
+app.get("/test", (req, res) => {
+    res.json({ "status": "success" })
 })
 
-app.post("/adminSignup",(req,res)=>{
-    let input=req.body
-    let hashedpassword=bcrypt.hashSync(input.password,10)
+app.post("/adminSignup", (req, res) => {
+    let input = req.body
+    let hashedpassword = bcrypt.hashSync(input.password, 10)
     //console.log(hashedpassword)
-    input.password=hashedpassword
+    input.password = hashedpassword
     console.log(input)
-    let result=new loginModel(input)
+    let result = new loginModel(input)
     result.save()
-    res.json({"status":"success"})
+    res.json({ "status": "success" })
 })
 
-app.listen(8080,()=>{
+app.post("/adminSignin", (req,res) => {
+    let input = req.body
+    let result = loginModel.find({ username: input.username }).then(
+        (response) => {
+            if (response.length > 0) {
+                const validator = bcrypt.compareSync(input.password, response[0].password)
+                if (validator)
+                    {
+                    res.json({ "status": "success" })
+            } else {
+                res.json({ "status": "wrong password" })
+            }
+        } 
+            else {
+                    res.json({ "status": "Invalid Authentication" })
+            }
+        }
+).catch()
+})
+
+
+app.listen(8080, () => {
     console.log("Server started")
 })
